7337444b2034d71f00f25f19feb0f31a933235af
carus-cat <probo0405@gmail.com>
2025-12-31 13:44:10 +0900

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -100,7 +100,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>
                 <div class="card-footer">
                     <span class="d-day">${program.dDay}</span>
-                    <a href="detail.html?id=${program.id}" target="_blank" class="details-link">
+                    <a href="${program.link}" target="_blank" class="details-link">
                         자세히 보기
                         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
                     </a>
